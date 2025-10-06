e26f42751e481edba731212a23392885dae8c112
Austin Meredith <austin09017@gmail.com>
2025-10-06 18:05:50 -0500

    added .gitignore and added a few coments

--- src/test/java/AzureTest.java
+++ src/test/java/AzureTest.java
@@ -8,13 +8,13 @@ public class AzureTest
     {
         assertTrue(true);
 
-        
+        // createing a box and constructing a rolesetter instance 
         BoxSpec boxSpec = new BoxSpec(100, 100, 100, 1, 8);
         SetEdgeRoles roleSetter = new SetEdgeRoles();
-        Panel panel1 = new Panel("p-0", Panel.PanelRole.side);
+        Panel panel1 = new Panel("p-0", Panel.PanelRole.side); // creating a panel for testing
 
-        boxSpec.panels.add(panel1);
-        roleSetter.setRoles(boxSpec.panels);
+        boxSpec.panels.add(panel1); // adding panel to list 
+        roleSetter.setRoles(boxSpec.panels); // sets roles for panels
   
         assertEquals(Panel.PanelRole.side, panel1.role);
         assertEquals(Panel.EdgeRole.male, panel1.edges.get(0));
