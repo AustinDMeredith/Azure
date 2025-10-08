2926a3633cea721ec97a1557c83d696ccd8dc70a
Austin Meredith <austin09017@gmail.com>
2025-10-08 12:42:00 -0500

    added unit tests

--- src/test/java/AzureTest.java
+++ src/test/java/AzureTest.java
@@ -6,24 +6,32 @@ public class AzureTest
     @Test 
     public void firstTest()
     {
+        BoxSpec.height = 100;
+        BoxSpec.width = 100;
+        BoxSpec.depth = 100;
+        BoxSpec.tolerance = 2;
+        BoxSpec.teethPerEdge = 8;
+        double toothDepth = 3.175;       
+
+        Panel panel1 = new Panel("p-0", Panel.PanelRole.side);
+        BoxSpec.panels.add(panel1);
+        SetEdgeRoles.setRoles(BoxSpec.panels);
+
+        PathGen.generatePanelPath(panel1, BoxSpec.width, BoxSpec.height, BoxSpec.teethPerEdge, toothDepth);
+
         assertTrue(true);
 
-    BoxSpec.height = 100;
-    BoxSpec.width = 100;
-    BoxSpec.depth = 100;
-    BoxSpec.tolerance = 2;
-    BoxSpec.teethPerEdge = 8;
-    Panel panel1 = new Panel("p-0", Panel.PanelRole.side);
+        assertEquals(BoxSpec.height, 100, 0);
+        assertEquals(BoxSpec.width, 100, 0);
+        assertEquals(BoxSpec.depth, 100, 0);
+        assertEquals(BoxSpec.tolerance, 2, 0);
+        assertEquals(BoxSpec.teethPerEdge, 8, 0);
 
-    BoxSpec.panels.add(panel1);
-    SetEdgeRoles.setRoles(BoxSpec.panels);
+        assertEquals(BoxSpec.panels.get(0).edges.get(0), Panel.EdgeRole.male);
+        assertEquals(BoxSpec.panels.get(0).edges.get(1), Panel.EdgeRole.female);
+        assertEquals(BoxSpec.panels.get(0).edges.get(2), Panel.EdgeRole.male);
+        assertEquals(BoxSpec.panels.get(0).edges.get(3), Panel.EdgeRole.female);
 
-    int teethPerEdge = 8;          // from BoxSpec if you like:contentReference[oaicite:6]{index=6}
-    double toothDepth = 3.175;       // mm, tuned to material thickness/fit
-    double width = 100, height = 100;
-    
-    PathGen.generatePanelPath(panel1, width, height, teethPerEdge, toothDepth);
-
-
+        assertEquals(BoxSpec.panels.get(0).path, "M5.000 5.000 l6.250 0.000 l0.000 -3.175 l6.250 0.000 l-0.000 3.175 l6.250 0.000 l0.000 -3.175 l6.250 0.000 l-0.000 3.175 l6.250 0.000 l0.000 -3.175 l6.250 0.000 l-0.000 3.175 l6.250 0.000 l0.000 -3.175 l6.250 0.000 l-0.000 3.175 l6.250 0.000 l0.000 -3.175 l6.250 0.000 l-0.000 3.175 l6.250 0.000 l0.000 -3.175 l6.250 0.000 l-0.000 3.175 l6.250 0.000 l0.000 -3.175 l6.250 0.000 l-0.000 3.175 l6.250 0.000 l0.000 -3.175 l6.250 0.000 l-0.000 3.175 l3.175 0.000 l0.000 6.250 l-3.175 -0.000 l0.000 6.250 l3.175 0.000 l0.000 6.250 l-3.175 -0.000 l0.000 6.250 l3.175 0.000 l0.000 6.250 l-3.175 -0.000 l0.000 6.250 l3.175 0.000 l0.000 6.250 l-3.175 -0.000 l0.000 6.250 l3.175 0.000 l0.000 6.250 l-3.175 -0.000 l0.000 6.250 l3.175 0.000 l0.000 6.250 l-3.175 -0.000 l0.000 6.250 l3.175 0.000 l0.000 6.250 l-3.175 -0.000 l0.000 6.250 l3.175 0.000 l0.000 6.250 l-3.175 -0.000 l0.000 6.250 l-6.250 0.000 l0.000 3.175 l-6.250 0.000 l-0.000 -3.175 l-6.250 0.000 l0.000 3.175 l-6.250 0.000 l-0.000 -3.175 l-6.250 0.000 l0.000 3.175 l-6.250 0.000 l-0.000 -3.175 l-6.250 0.000 l0.000 3.175 l-6.250 0.000 l-0.000 -3.175 l-6.250 0.000 l0.000 3.175 l-6.250 0.000 l-0.000 -3.175 l-6.250 0.000 l0.000 3.175 l-6.250 0.000 l-0.000 -3.175 l-6.250 0.000 l0.000 3.175 l-6.250 0.000 l-0.000 -3.175 l-6.250 0.000 l0.000 3.175 l-6.250 0.000 l-0.000 -3.175 l-3.175 0.000 l0.000 -6.250 l3.175 -0.000 l0.000 -6.250 l-3.175 0.000 l0.000 -6.250 l3.175 -0.000 l0.000 -6.250 l-3.175 0.000 l0.000 -6.250 l3.175 -0.000 l0.000 -6.250 l-3.175 0.000 l0.000 -6.250 l3.175 -0.000 l0.000 -6.250 l-3.175 0.000 l0.000 -6.250 l3.175 -0.000 l0.000 -6.250 l-3.175 0.000 l0.000 -6.250 l3.175 -0.000 l0.000 -6.250 l-3.175 0.000 l0.000 -6.250 l3.175 -0.000 l0.000 -6.250 l-3.175 0.000 l0.000 -6.250 l3.175 -0.000 l0.000 -6.250 Z");
     }    
 }
