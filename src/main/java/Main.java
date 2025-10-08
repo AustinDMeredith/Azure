02a10c8625f32c2f889578aafa4a3ea03f7c1ee9
Austin Meredith <austin09017@gmail.com>
2025-10-08 12:15:37 -0500

    finished the demo for thursday, just need to add unit tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,18 @@
 public class Main {
   public static void main (String args[]) {
-    BoxSpec.highth = 100;
+    BoxSpec.height = 100;
     BoxSpec.width = 100;
     BoxSpec.depth = 100;
     BoxSpec.tolerance = 2;
     BoxSpec.teethPerEdge = 8;
-    Panel panel1 = new Panel("p-0", Panel.PanelRole.side);
+    double toothDepth = 3.175;       // mm, tuned to material thickness/fit
 
+    Panel panel1 = new Panel("p-0", Panel.PanelRole.side);
     BoxSpec.panels.add(panel1);
     SetEdgeRoles.setRoles(BoxSpec.panels);
 
-    int teethPerEdge = 8;          // from BoxSpec if you like:contentReference[oaicite:6]{index=6}
-    double toothDepth = 3.175;       // mm, tuned to material thickness/fit
-    double width = 100, height = 100;
     
-    PathGen.generatePanelPath(panel1, width, height, teethPerEdge, toothDepth);
-
-    System.out.print(panel1.path);
+    PathGen.generatePanelPath(panel1, BoxSpec.width, BoxSpec.height, BoxSpec.teethPerEdge, toothDepth);
+    SvgGen.generateFile();
   }  
 }
--- src/main/java/SvgGen.java
+++ src/main/java/SvgGen.java
@@ -3,7 +3,45 @@
  * Description: This class will generate the svg. We will probably have a .svg that has all the boilerplate that we just manipulate
  *              the stuff we need to change.
  * */
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class SvgGen {
+  static String line1 = "<?xml version='1.0' encoding='utf-8'?>\n";
+  static String line2 = """ 
+    <svg height="1000mm" viewBox="0.0 0.0 1000 1000" width="1000mm"
+     xmlns:xlink="http://www.w3.org/1999/xlink">\n
+  """;
+  
+  static String line3 = "<g id=\"" + BoxSpec.panels.get(0).id + "\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">";
+  static String line4 = "<path d=\"" + BoxSpec.panels.get(0).path + "\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.10\" />";
+  static String line5 = "</g>";
+  static String line6 = "</svg>";
 
+  static public void generateFile () {
+    try {
+      FileWriter clear = new FileWriter("box.svg");
+      clear.write("");
+      clear.close();
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    
+    try (FileWriter writer = new FileWriter("box.svg", true)) {
+      writer.write(line1);
+      writer.write(line2);
+      writer.write(line3);
+      writer.write(line4);
+      writer.write(line5);
+      writer.write(line6);
+      System.out.println("Successfully wrote to box.svg");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+  
 }
+
+
