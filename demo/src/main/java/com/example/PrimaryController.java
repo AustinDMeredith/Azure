652bbe00d30aaaba6989acea4acb0c0be6ea384c
devsal34 <dss22c@acu.edu>
2025-11-10 16:24:03 -0600

    worked on color and added images to the boxes

--- demo/src/main/java/com/example/PrimaryController.java
+++ demo/src/main/java/com/example/PrimaryController.java
@@ -2,21 +2,22 @@ package com.example;
 
 import javafx.fxml.FXML;
 import java.io.IOException;
+import javafx.scene.input.MouseEvent;
 
 public class PrimaryController {
 
     @FXML
-    private void handleBox1Click() throws IOException {
+    private void handleRegularBoxClick(MouseEvent event) throws IOException {
         App.setRoot("secondary1");
     }
 
     @FXML
-    private void handleBox2Click() throws IOException {
+    private void handleBasedBoxClick(MouseEvent event) throws IOException {
         App.setRoot("secondary2");
     }
 
     @FXML
-    private void handleBox3Click() throws IOException {
+    private void handleDieBoxClick(MouseEvent event) throws IOException {
         App.setRoot("secondary3");
     }
 }
